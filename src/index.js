815d831f15ddfa4c6f59cd7068955e2b090b0b44
GeoBo <bozhys@gmail.com>
2021-12-31 18:44:51 +0300

    feat: update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let result ='';
+    n = Math.abs(n);
+    n = n.toString ();
+
+    for (let i=0; i<n.length; i++) {
+        result += n[n.length - i - 1];
+    }
+    return +result;
 }
+
